949b43a13fb73c3d064c865ad632848cef92b922
ZernoBilly <heikki.kurppa@gmail.com>
2021-06-09 22:22:37 +0300

    try to fix payload too large error on POST request

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
 import cors from "cors";
 
 import cardsRoutes from "./routes/cards.js";
@@ -9,11 +8,11 @@ import cardsRoutes from "./routes/cards.js";
 dotenv.config();
 const app = express();
 
-app.use(express.json());
-
-app.get("/", (req, res) => res.send("Hello from homepage!"));
-
 app.use(cors());
+app.use(express.json());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
 app.use("/cards", cardsRoutes);
 
 const PORT = process.env.PORT;
